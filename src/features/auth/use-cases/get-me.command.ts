6e4068ec7a2914affdbce33c9bfb70d15386aa23
Satyxa <satyan20003@gmail.com>
2025-08-05 11:44:38 +0200

    fix get me and logout use cases

--- src/features/auth/use-cases/get-me.command.ts
+++ src/features/auth/use-cases/get-me.command.ts
@@ -2,6 +2,7 @@ import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { UserRepository } from '../db/users.repository';
 import { JwtService } from '../helpers/jwt-service';
 import { UnauthorizedException } from '@nestjs/common';
+import { TokenExpiredRepository } from '../db/token.repository';
 
 export class GetMeCommand {
   constructor(public payload: { accessToken: string }) {}
@@ -11,10 +12,16 @@ export class GetMeCommand {
 export class GetMeHandler implements ICommandHandler<GetMeCommand> {
   constructor(
     private userRepository: UserRepository,
+    private tokenExpiredRepository: TokenExpiredRepository,
     private jwtService: JwtService,
   ) {}
 
   async execute({ payload }: GetMeCommand) {
+    const isTokenExpired = await this.tokenExpiredRepository.getToken(
+      payload.accessToken,
+    );
+    if (isTokenExpired) throw new UnauthorizedException(isTokenExpired);
+
     const tokenPayload = this.jwtService.getResultByToken(payload.accessToken);
     if (!tokenPayload) throw new UnauthorizedException('Unauthorized');
     const user = await this.userRepository.getUserById(tokenPayload.id);
--- src/features/auth/use-cases/logout.command.ts
+++ src/features/auth/use-cases/logout.command.ts
@@ -1,6 +1,7 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { TokenExpiredRepository } from '../db/token.repository';
 import { LogoutRequest } from '../swagger.validation/models/logout-model';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class LogoutCommand {
   constructor(public payload: LogoutRequest) {}
@@ -11,6 +12,10 @@ export class LogoutHandler implements ICommandHandler<LogoutCommand> {
   constructor(private tokenExpiredRepository: TokenExpiredRepository) {}
 
   async execute({ payload }: LogoutCommand) {
+    const isTokenAlreadyExpired = await this.tokenExpiredRepository.getToken(
+      payload.token,
+    );
+    if (isTokenAlreadyExpired) throw new UnauthorizedException('Unauthorized');
     await this.tokenExpiredRepository.expireToken(payload.token);
   }
 }
